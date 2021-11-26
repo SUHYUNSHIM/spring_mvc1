f0c24bead00e6facfbc4d8b06c27a985dbc6bc45
suhyunshim <lisa9963suhyun@gmail.com>
2021-11-26 23:29:55 +0900

    comment

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @ServletComponentScan //서블릿 자동 등록..
-@SpringBootApplication
+@SpringBootApplication //시작 클래스입을 알린다.
 public class ServletApplication {
 
 	public static void main(String[] args) {
