e5b0e8118c3984313098cc87a040bc3f3711e768
suhyunshim <lisa9963suhyun@gmail.com>
2021-12-02 23:47:58 +0900

    front controller v1 comment

--- src/main/java/hello/servlet/web/frontcontroller/v1/ControllerV1.java
+++ src/main/java/hello/servlet/web/frontcontroller/v1/ControllerV1.java
@@ -5,6 +5,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public interface ControllerV1 {
+public interface ControllerV1 { //controller interface. 메소드 구현을 통해서 controller를 모아서 호출
     void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
 }
