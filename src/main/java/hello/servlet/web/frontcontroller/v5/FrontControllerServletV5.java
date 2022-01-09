4dae57e8060d78fe373a3ddcd3afcb3d3d5fd42b
suhyunshim <lisa9963suhyun@gmail.com>
2022-01-09 11:42:21 +0900

    adapter handler, servlet. v4

--- src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
+++ src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -6,7 +6,11 @@ import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
 import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
 import hello.servlet.web.frontcontroller.v4.ControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
 import hello.servlet.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
+import hello.servlet.web.frontcontroller.v5.adapter.ControllerV4HandlerAdapter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -36,22 +40,28 @@ public class FrontControllerServletV5 extends HttpServlet {
           handlerMappingMap.put("/front-controller/v5/v3/members/new-form",new MemberFormControllerV3());
           handlerMappingMap.put("/front-controller/v5/v3/members/save",new MemberSaveControllerV3());
           handlerMappingMap.put("/front-controller/v5/v3/members",new MemberListControllerV3());
+
+          //v4 추가
+          handlerMappingMap.put("/front-controller/v5/v4/members/new-form",new MemberFormControllerV4());
+          handlerMappingMap.put("/front-controller/v5/v4/members/save",new MemberSaveControllerV4());
+          handlerMappingMap.put("/front-controller/v5/v4/members",new MemberListControllerV4());
      }
 
      private void initHandlerAdapters() {
           handlerAdapters.add(new ControllerV3HandlerAdapter());
+          handlerAdapters.add(new ControllerV4HandlerAdapter()); //v4 추가
      }
 
      protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-          Object handler = getHandler(request); //handler를 찾는다.
+          Object handler = getHandler(request); //handler를 찾는다. ex) MemberFormControllerV4 반환
           
           if (handler == null){
                response.setStatus(HttpServletResponse.SC_NOT_FOUND);
                return;
           }
 
-          MyHandlerAdapter adapter = getHandlerAdapter(handler); //handler adapter를 찾는다
-          ModelView mv = adapter.handle(request,response,handler);
+          MyHandlerAdapter adapter = getHandlerAdapter(handler); //adapter를 찾는다 . 반환 받은 ControllerV4HandlerAdpater.
+          ModelView mv = adapter.handle(request,response,handler); //modelview 반환 받음.
 
           String viewName = mv.getViewName();
           MyView view = viewResolver(viewName);
@@ -65,8 +75,9 @@ public class FrontControllerServletV5 extends HttpServlet {
      }
 
      private MyHandlerAdapter getHandlerAdapter(Object handler) {
+         //MemberFormControllerV4 . handler 값.
           for (MyHandlerAdapter adapter : handlerAdapters) {
-               if (adapter.supports(handler)){
+               if (adapter.supports(handler)){  //v3, v4 맞는지 리스트 확인 후 adapter 반환. ControllerV4HandlerAdapter 반환.
                     return adapter;
                }
           }
