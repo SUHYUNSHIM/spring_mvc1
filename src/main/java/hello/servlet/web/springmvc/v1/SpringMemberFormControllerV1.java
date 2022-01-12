09f51160aa8aee72f56fae74c8b2aa2841cd2275
suhyunshim <lisa9963suhyun@gmail.com>
2022-01-13 00:12:14 +0900

    회원저장 ver1

--- src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -1,11 +1,13 @@
 package hello.servlet.web.springmvc.v1;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-@Component
-@RequestMapping
+/*@Component
+@RequestMapping*/
+@Controller
 public class SpringMemberFormControllerV1 {
     @RequestMapping("/springmvc/v1/members/new-form")
     public ModelAndView process(){
